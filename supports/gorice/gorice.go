3dba33e3bd7d2d94c2ee06653c7629633967e8b0
foolin <ling@liufu.me>
2019-05-07 19:04:00 +0800

    update comments

--- supports/gorice/gorice.go
+++ supports/gorice/gorice.go
@@ -1,21 +1,16 @@
 package gorice
 
 import (
-	"github.com/GeertJohan/go.rice"
 	"github.com/foolin/goview"
 )
 
-/**
-New gin template engine, default views root.
-*/
+// New gin template engine, default views root.
 func New(viewsRootBox *rice.Box) *goview.ViewEngine {
 	return NewWithConfig(viewsRootBox, goview.DefaultConfig)
 }
 
-/**
-New gin template engine
-Important!!! The viewsRootBox's name and config.Root must be consistent.
-*/
+// New gin template engine
+// Important!!! The viewsRootBox's name and config.Root must be consistent.
 func NewWithConfig(viewsRootBox *rice.Box, config goview.Config) *goview.ViewEngine {
 	config.Root = viewsRootBox.Name()
 	engine := goview.New(config)
@@ -23,9 +18,7 @@ func NewWithConfig(viewsRootBox *rice.Box, config goview.Config) *goview.ViewEng
 	return engine
 }
 
-/**
-Support go.rice file handler
-*/
+// FileHandler Support go.rice file handler
 func FileHandler(viewsRootBox *rice.Box) goview.FileHandler {
 	return func(config goview.Config, tplFile string) (content string, err error) {
 		// get file contents as string
