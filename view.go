803ad1c488df9a5385fe905d15540dc0c614de73
foolin <ling@liufu.me>
2019-05-07 19:39:14 +0800

    update comments

--- view.go
+++ view.go
@@ -1,5 +1,4 @@
-/*
-package goview based on golang html/template for building Go web application
+/* package goview based on golang html/template for building Go web application
 
 Goview is a lightweight, minimalist and idiomatic template library
 based on golang html/template for building Go web application.
