4ef50ed33dad99cee52e62ca5e6a124add3761bd
foolin <ling@liufu.me>
2019-05-08 22:48:50 +0800

    add test render

--- instance_test.go
+++ instance_test.go
@@ -8,6 +8,18 @@ import (
 	"testing"
 )
 
+func TestDefault(t *testing.T) {
+	recorder := httptest.NewRecorder()
+	err := Render(recorder, http.StatusOK, "index", M{})
+	//expect like this info:
+	// "error: ViewEngine render read name:layouts/master,  path:/foolin/goview/views/layouts/master.html, error: open /foolin/goview/views/layouts/master.html: The system cannot find the path specified"
+	if err == nil {
+		t.Error("render is ok?")
+	} else {
+		t.Logf("expect error: %v", err)
+	}
+}
+
 func TestRender(t *testing.T) {
 
 	engine := New(Config{
@@ -32,9 +44,13 @@ func TestRender(t *testing.T) {
 		t.Errorf("render error: %v", err)
 		return
 	}
+	assertRecorder(t, recorder, http.StatusOK, expect)
+}
+
+func assertRecorder(t *testing.T, recorder *httptest.ResponseRecorder, expectStatusCode int, expectOut string) {
 	result := recorder.Result()
-	if result.StatusCode != http.StatusOK {
-		t.Errorf("actual: %v, expect: %v", result.StatusCode, http.StatusOK)
+	if result.StatusCode != expectStatusCode {
+		t.Errorf("actual: %v, expect: %v", result.StatusCode, expectStatusCode)
 	}
 	resultBytes, err := ioutil.ReadAll(result.Body)
 	if err != nil {
@@ -42,7 +58,7 @@ func TestRender(t *testing.T) {
 		return
 	}
 	val := string(resultBytes)
-	if val != expect {
-		t.Errorf("actual: %v, expect: %v", val, expect)
+	if val != expectOut {
+		t.Errorf("actual: %v, expect: %v", val, expectOut)
 	}
 }
